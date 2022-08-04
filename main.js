db771185c5a634682357f708735f68d5510b387f
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-08-04 11:41:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function pressA() {
+    document.getElementById("iA").innerText = "You like butterflies!"
+}
+
+function pressB() {
+    document.getElementById("iB").innerText = "You like unicorns!"
+}
